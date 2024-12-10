6bddf05b50d55941cab4e47a9267d650769ad52d
Jamie Turner <jamwt@convex.dev>
2024-12-09 21:01:58 -0800

    formatting.

--- app/components/MovieDetail.tsx
+++ app/components/MovieDetail.tsx
@@ -31,10 +31,10 @@ const MovieDetail: React.FC<MovieProps> = ({ tid, title, year, length }) => {
         <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 font-script leading-tight mb-1">
           {title}
         </h3>
-        <div className="flex space-x-2 text-md text-gold-leaf font-script">
-          <span>© {year}</span>
+        <div className="flex space-x-2 text-md text-gold-leaf">
+          <span>©{year}</span>
           <span>•</span>
-          <span>{length}</span>
+          <span>{length} minutes</span>
         </div>
       </div>
     </li>
